c8c53989f6066586c5e86af0981c5a8a3a409b8a
SandunTBandara <devocksandu99@gmail.com>
2024-03-28 17:40:32 +0530

    Initial Commit

---
+++ ClothingApplication/ClothingApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClothingApplicationApp.swift
+//  ClothingApplication
+//
+//  Created by Sandun Bandara on 2024-03-28.
+//
+
+import SwiftUI
+
+@main
+struct ClothingApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
